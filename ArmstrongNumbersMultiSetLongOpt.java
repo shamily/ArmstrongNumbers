84d7669ababd47feba3613a12759cb264692295c
shamily <shamil.yagiyayev@gmail.com>
2016-06-22 19:44:15 +0300

    Updated optimized Multi Set (Long) solution

--- ArmstrongNumbersMultiSetLongOpt.java
+++ ArmstrongNumbersMultiSetLongOpt.java
@@ -3,11 +3,19 @@ import java.util.Collections;
 import java.util.List;
 
 public class ArmstrongNumbersMultiSetLongOpt {
+    private static int N;
+    private static int[] digitsMultiSet;
+    private static int[] testMultiSet;
 
-    private static int N; // Current length
-    private static long[][] pows = new long[10][20];
+    private static List<Long> results;
+    private static long maxPow;
+    private static long minPow;
 
-    private static void genPows() {
+    private static long[][] pows;
+
+    private static void genPows(int N) {
+        if (N > 20) throw new IllegalArgumentException();
+        pows = new long[10][N + 1];
         for (int i = 0; i < pows.length; i++) {
             long p = 1;
             for (int j = 0; j < pows[i].length; j++) {
@@ -17,36 +25,23 @@ public class ArmstrongNumbersMultiSetLongOpt {
         }
     }
 
-    private static final int BASE = 10; // decimal base
-    private static int[] digits = new int[BASE];
-    private static int[] pd = new int[BASE];
-
-    private static List<Long> results = new ArrayList<>();
-    private static long maxPow;
-    private static long minPow;
-
-    private static void preparePow(int N) {
-        minPow = (long) Math.pow(10, N - 1);
-        maxPow = (long) Math.pow(10, N);
-    }
-
     private static boolean check(long pow) {
         if (pow >= maxPow) return false;
         if (pow < minPow) return false;
 
-        for (int i = 0; i < BASE; i++) {
-            pd[i] = 0;
+        for (int i = 0; i < 10; i++) {
+            testMultiSet[i] = 0;
         }
 
         while (pow > 0) {
             int i = (int) (pow % 10);
-            pd[i]++;
-            if (pd[i] > digits[i]) return false;
+            testMultiSet[i]++;
+            if (testMultiSet[i] > digitsMultiSet[i]) return false;
             pow = pow / 10;
         }
 
-        for (int i = 0; i < BASE; i++) {
-            if (pd[i] != digits[i]) return false;
+        for (int i = 0; i < 10; i++) {
+            if (testMultiSet[i] != digitsMultiSet[i]) return false;
         }
 
         return true;
@@ -61,7 +56,7 @@ public class ArmstrongNumbersMultiSetLongOpt {
         }
 
         if (digit == 0) {
-            digits[digit] = unused;
+            digitsMultiSet[digit] = unused;
             search(digit - 1, 0, pow + unused * pows[digit][N]);
         } else {
             // Check if we can generate more than minimum
@@ -69,7 +64,7 @@ public class ArmstrongNumbersMultiSetLongOpt {
 
             long p = pow;
             for (int i = 0; i <= unused; i++) {
-                digits[digit] = i;
+                digitsMultiSet[digit] = i;
                 search(digit - 1, unused - i, p);
                 if (i != unused) {
                     p += pows[digit][N];
@@ -80,23 +75,32 @@ public class ArmstrongNumbersMultiSetLongOpt {
         }
     }
 
-    public static void main(String[] args) {
-        Long tStart = System.currentTimeMillis();
+    public static List<Long> generate(int maxN) {
+        if (maxN >= 20) throw new IllegalArgumentException();
 
-        genPows();
-        for (N = 0; N < 10; N++) {
-            preparePow(N);
-            search(BASE - 1, N, 0);
+        genPows(maxN);
+        results = new ArrayList<>();
+        digitsMultiSet = new int[10];
+        testMultiSet = new int[10];
+
+        for (N = 1; N <= maxN; N++) {
+            minPow = (long) Math.pow(10, N - 1);
+            maxPow = (long) Math.pow(10, N);
+
+            search(10 - 1, N, 0);
         }
 
         Collections.sort(results);
 
-        Long tFinish = System.currentTimeMillis();
+        return results;
+    }
 
-        System.out.println("Time  = " + (tFinish - tStart));
-        System.out.println(String.format("Used JVM Memory: %.3f Mb", (double) (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1048576));
-
-        System.out.println(results.size());
-        System.out.println(results);
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        List<Long> list = generate(9);
+        long finish = System.currentTimeMillis();
+        System.out.println("Time consumed: " + (finish - start) + " ms");
+        System.out.println(list.size());
+        System.out.println(list);
     }
 }
